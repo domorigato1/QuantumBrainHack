b672b76d947d074128d90d9f23c11afdc91cfca6
domorigato1 <dominicmargherio@gmail.com>
2025-02-22 13:24:39 -0600

    Added BTC price fetch to app.py

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-print("SavageHealth rocks")
+import requests
+def get_btc_price():
+    response = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd")
+    data = response.json()
+    price = data["bitcoin"]["usd"]
+    print(f"BTC Price: ${price}")
+get_btc_price()
